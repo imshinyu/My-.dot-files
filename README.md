89e1081141bdfcf56e93b720ba873aed4a90c4aa
imshinyu <khalil.hasnaoui15@gmail.com>
2025-05-03 00:32:57 +0100

    adding the stuff

---
+++ README.md
@@ -0,0 +1,2 @@
+# My-.dot-files
+# My-.dot-files
---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    mode = { "n", "i", "v" },
+    "<C-s>",
+    "<cmd>w<cr><esc>",
+    desc = "Save file",
+  },
+  {
+    mode = { "n", "i", "v" },
+    "<C-q>",
+    "<cmd>q!<cr>",
+    desc = "Save (if modified) and quit",
+  },
+}
